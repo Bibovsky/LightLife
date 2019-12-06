85252b7712a04a75d3c14144878bdc2054ee1a39
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-12-06 22:53:24 +0300

    mask lib

--- app/src/main/java/com/example/finalproject/AddEventActivity.kt
+++ app/src/main/java/com/example/finalproject/AddEventActivity.kt
@@ -1,16 +1,20 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.finalproject.models.DiaryModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import java.util.*
+import ru.tinkoff.decoro.MaskImpl
+import ru.tinkoff.decoro.parser.PhoneNumberUnderscoreSlotsParser
+import ru.tinkoff.decoro.watchers.FormatWatcher
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher
+
 
 class AddEventActivity : AppCompatActivity() {
     private lateinit var mDatebase: FirebaseDatabase
@@ -33,9 +37,22 @@ class AddEventActivity : AppCompatActivity() {
         dateText = findViewById(R.id.dateEdit)
         timeText = findViewById(R.id.timeEdit)
         descText = findViewById(R.id.descEdit)
+        val slotsDate = PhoneNumberUnderscoreSlotsParser().parseSlots("__.__.____")
+        val slotsTime = PhoneNumberUnderscoreSlotsParser().parseSlots("__:__")
+        val formatWatcherDate: FormatWatcher = MaskFormatWatcher(
+            MaskImpl.createTerminated(slotsDate)
+        )
+        val formatWatcherTime: FormatWatcher = MaskFormatWatcher(
+            MaskImpl.createTerminated(slotsTime)
+        )
+        formatWatcherDate.installOn(dateText)
+        formatWatcherTime.installOn(timeText)
+
+
 
         saveBtn.setOnClickListener {
-            if(dateText.text.toString()!="" && timeText.text.toString()!="" && descText.text.toString()!="") {
+            if(dateText.text.toString()!="" && timeText.text.toString()!="" && descText.text.toString()!=""
+                && dateText.text.toString().length==10 &&  timeText.text.toString().length==5) {
                 mReference.child("events").push().setValue(
                     DiaryModel(
                         dateText.text.toString(),
